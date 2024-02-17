a37b89f834f7fb586cfd7cca46796710042a8640
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-02-17 18:45:35 +0100

    001 and 002 lessons complete

--- 01_OOP/app.js
+++ 01_OOP/app.js
@@ -35,6 +35,16 @@ let myTri = {
 
 // CLASSES BASICS
 class Triangle {
+  constructor(a, b) {
+    if (!Number.isFinite(a) || a <= 0) {
+      throw new Error("Invalid");
+    }
+    if (!Number.isFinite(b) || b <= 0) {
+      throw new Error("Invalid");
+    }
+    this.a = a;
+    this.b = b;
+  }
   getArea() {
     return (this.a * this.b) / 2;
   }
@@ -42,18 +52,82 @@ class Triangle {
   getHypotenuse() {
     return Math.sqrt(this.a ^ (2 + this.b) ^ 2);
   }
+
+  sayHi() {
+    return `The triangle with side A of ${this.a} and side B of ${
+      this.b
+    } and with an area of ${this.getArea()}`;
+  }
 }
 
 // CLASSES WITH  CONSTRUCTOR
 class TriangleWithConstructor {
-    constructor(a,b){
-        if (!Number.isFinite(a) || a<=0){
-            throw new Error("Invalid")
-        }
-        if (!Number.isFinite(b) || b<=0){
-            throw new Error("Invalid")
-        }
-        this.a = a;
-        this.b = b
+  constructor(a, b) {
+    if (!Number.isFinite(a) || a <= 0) {
+      throw new Error("Invalid");
     }
+    if (!Number.isFinite(b) || b <= 0) {
+      throw new Error("Invalid");
+    }
+    this.a = a;
+    this.b = b;
+  }
 }
+
+class shyTriangle extends Triangle {
+  describe() {
+    return "(runs and hides)";
+  }
+}
+class ColorTriangle extends Triangle {
+  constructor(a, b, color) {
+    super(a, b);
+    this.color = color;
+  }
+}
+
+// Static Properties example
+
+class Cat {
+  constructor(name, breed) {
+    this.name = name;
+    this.species = breed;
+  }
+
+  // static methods
+  static meow() {
+    return `The ${this.breed} cat says MEOW!!`;
+  }
+  // it does not vary so we put a static property
+  static species = "flelis catus";
+}
+
+class ArrayUtils {
+  constructor() {
+    throw Error("ArrayUtils cannot be instantiated.");
+  }
+  static average(array) {
+    if (Array.isArray(array)) {
+      let sum = 0;
+      for (var i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+      }
+      return sum / array.length;
+    }
+  }
+
+  static max(array) {
+    if (Array.isArray(array)) {
+      let maxNumber = 0;
+      for (var i = 0; i < array.length; i++) {
+        if (maxNumber < array[i]) {
+          maxNumber = array[i];
+        }
+      }
+
+      return maxNumber;
+    }
+  }
+}
+
+console.log(ArrayUtils.max([1, 2, 3, 4]));
--- 01_OOP/codeAlong.js
+++ 01_OOP/codeAlong.js
@@ -1,1 +1,15 @@
 // CODE ALONG
+class BankAccount {
+    constructor(balance = 0,accountHolder,accountNumber){
+        this.balance = balance;
+        this.accountHolder = accountHolder
+        this.accountNumber = accountNumber
+    }
+                                
+    deposit (amt) {
+        this.balance =  this.balance + amt
+    }
+    withdraw (amt) {
+        this.balance =  this.balance - amt
+    }
+}
